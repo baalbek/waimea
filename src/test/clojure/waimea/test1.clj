e11ebc8a057c6a1fc66df9d729c0e9cd28611834
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2012-10-15 23:04:35 +0200

    More tests, moved weighted-values from Taharoa

--- src/test/clojure/waimea/test1.clj
+++ src/test/clojure/waimea/test1.clj
@@ -1,7 +1,9 @@
 (ns waimea.test1
     (:use clojure.contrib.test-is
-          waimea.testehlers))
+          waimea.testehlers
+          waimea.testutils))
 
 (run-tests 'waimea.testehlers)
+(run-tests 'waimea.testutils)
 
     
--- src/test/clojure/waimea/testutils.clj
+++ src/test/clojure/waimea/testutils.clj
@@ -1,5 +1,13 @@
 (ns waimea.testutils
-    (:require [waimea.utils :as C])
+  (:require
+    [waimea.utils :as U]
+    [waimea.filters.filterutils :as FU])
     (:use clojure.test))
 
 (is (= 1 1))
+
+(def a [1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0])
+(def b [11.0 12.0 13.0 14.0 15.0 16.0 17.0 18.0 19.0 20.0])
+
+(let [ab (FU/weighted-values a b 3)]
+  (is (= (count ab) 8)))
