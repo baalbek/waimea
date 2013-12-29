baa0cb7b608b8dbff0c0019b7ff01fce398183e2
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-12-29 16:54:24 +0100

    Gradle, removed src/java

--- src/main/clojure/waimea/utils/logservice.clj
+++ src/main/clojure/waimea/utils/logservice.clj
@@ -1,8 +1,20 @@
 (ns waimea.utils.logservice
   (:import
-    [org.apache.log4j Logger]))
+    [org.apache.log4j Logger PropertyConfigurator]
+    [org.apache.commons.logging LogFactory]
+    [java.util Properties]))
 
 (def logger ^Logger (Logger/getLogger "waimea.utils"))
+
+(defn initLog4j [& [xml-name]]
+  (let [cur-xml-name (if (nil? xml-name) "/log4j.xml" xml-name)
+        lf (LogFactory/getFactory)
+        props (Properties.)
+        clazz (.getClass props)
+        resource (.getResourceAsStream clazz cur-xml-name)]
+    (.setAttribute lf "org.apache.commons.logging.Log" "org.apache.commons.logging.impl.NoOpLog")
+    (.load props resource)
+    (PropertyConfigurator/configure props)))
 
 (defn fatal [msg]
   (.fatal ^Logger logger msg))
