0c51e2d2069a6ddc71820d82d39f01063058dcf5
rcs <rolf.c.stadheim@gmail.com>
2015-01-24 10:49:04 +0100

    Moved ehlers

--- src/main/clojure/waimea/utils/commonutils.clj
+++ src/main/clojure/waimea/utils/commonutils.clj
@@ -47,17 +47,6 @@
   (let [[y m d] (map #(Integer. ^String %) (split #"-" s))]
     (new-date y m d)))
 
-(defn calc-smooth [data]
-  (loop [result (vec (take 4 data))
-         datax (drop 1 data)]
-    (let [[a b c d] (take 4 datax)]
-      (if (nil? d)
-        result
-        (recur (conj result (/ (+ a (* 2.0 b) (* 2.0 c) d) 6.0)) (rest datax))))))
-
-(defn calc-alpha [days]
-  (/ 2.0 (+ days 1.0)))
-
 (defn calc-days [alpha]
   (- (/ 2.0 alpha) 1.0))
 
--- src/test/clojure/waimea/test1.clj
+++
@@ -1,9 +0,0 @@
-(ns waimea.test1
-    (:use clojure.contrib.test-is
-          waimea.testehlers
-          waimea.testutils))
-
-(run-tests 'waimea.testehlers)
-(run-tests 'waimea.testutils)
-
-    
